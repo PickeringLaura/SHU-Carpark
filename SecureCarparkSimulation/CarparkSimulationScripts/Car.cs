41944cf9331fe71ec25f6840368b2320ba1ee357
Unknown <pickering.laura@ymail.com>
2018-01-22 19:07:27 +0000

    Created Car Class
    created a new car class for creation of new cars into the application.

---
+++ SecureCarparkSimulation/CarparkSimulationScripts/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureCarparkSimulation.CarparkSimulationScripts
+{
+    class Car
+    {
+        string _reg;
+        string _password;
+
+        public Car(string _reg, string _password)
+        {
+            _reg = "";
+            _password = "";
+        }
+
+        public void SetReg(string reg)
+        {
+            _reg = reg;
+        }
+
+        public void SetPass(string pass)
+        {
+            _password = pass;
+        }
+
+        public bool ValidateInformation(string reg, string password)
+        {
+            if (_reg.Equals(reg) && _password.Equals(password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- SecureCarparkSimulation/CarparkSimulationScripts/Space.cs
+++ SecureCarparkSimulation/CarparkSimulationScripts/Space.cs
@@ -11,43 +11,17 @@ namespace SecureCarparkSimulation.CarparkSimulationScripts
         int _id;
         int _floor;
         bool _allocated;
-        string _reg;
-        string _password;
 
         public Space(int id, int floor)
         {
             _id = id;
             _floor = floor;
             _allocated = false;
-            _reg = "";
-            _password = "";
-        }
-
-        public void SetReg(string reg)
-        {
-            _reg = reg;
-        }
-
-        public void SetPass(string pass)
-        {
-            _password = pass;
         }
 
         public bool IsAllocated()
         {
             return _allocated;
-        }
-
-        public bool ValidateInformation(string reg, string password)
-        {
-            if(_reg.Equals(reg) && _password.Equals(password))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
         }
     }
 }
